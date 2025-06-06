9f1a658b19e23bfca265000bbe984909b002bdb1
kevin903476 <73252529+kevin903476@users.noreply.github.com>
2025-06-06 16:55:07 -0600

    fix: add file upload middleware to register route in officialsRoutes

--- routes/officialsRoutes.js
+++ routes/officialsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const officialsController = require('../controllers/officialsController');
 
 router.get('/getOfficials', officialsController.getAllOfficials);
-router.post('/register', officialsController.registerOfficial);
+router.post("/register", upload.single("foto"), officialsController.registerOfficial);
 router.put('/update', officialsController.updateOfficial);
 
 module.exports = router;
